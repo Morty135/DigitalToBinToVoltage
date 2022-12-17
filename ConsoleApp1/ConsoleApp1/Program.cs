af9568a86df9b9c0b27e2357b0a4eb662d0cd38a
Morty135 <59707384+Morty135@users.noreply.github.com>
2022-12-17 21:17:41 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool exit = false;
+            while(exit == false)
+            {
+                Console.WriteLine("input number or exit");
+                string Input = Console.ReadLine();
+                int Loading = 0;
+                Console.Clear();
+                Console.WriteLine("Loading ");
+                if (Input == "exit")
+                {
+                    exit = true;
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    exit = false;
+                }
+                while (99 > Loading)
+                {
+                    Random rnd = new Random();
+                    Loading = Loading + rnd.Next(1, 10);
+                    if(Loading > 100)
+                    {
+                        Loading = 100;
+                    }
+                    Console.Write("\r{0}%   ", Loading + "%");
+                    
+                    await Task.Delay(100);
+                }
+                Console.Clear();
+                Console.WriteLine(string.Empty);
+                int InputInt = Convert.ToInt32(Input, 10);
+                int StaticInput = InputInt;
+
+                string result = String.Empty;
+                while (InputInt > 0)
+                {
+                    int remainder = InputInt % 2;
+                    InputInt /= 2;
+                    result = remainder.ToString() + result;
+                }
+                Console.WriteLine("Binary output:" + result);
+                float Output = StaticInput / 1 * -1.25f;
+                Console.WriteLine("Voltage output:" + Output + "V");
+            
+            }
+        }
+    }
+    
+}
